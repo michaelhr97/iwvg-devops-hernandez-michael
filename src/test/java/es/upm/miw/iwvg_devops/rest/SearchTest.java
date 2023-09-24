2466b0a8f4cc7f9d862281cefa5b0a85ddedde9b
Michael Hernandez <michaelhernandezrala@gmail.com>
2023-09-24 11:26:30 +0200

    Implementing method findUserIdByAllProperFraction and adding tests #23

--- src/test/java/es/upm/miw/iwvg_devops/rest/SearchTest.java
+++ src/test/java/es/upm/miw/iwvg_devops/rest/SearchTest.java
@@ -4,6 +4,9 @@ import es.upm.miw.iwvg_devops.code.Fraction;
 import es.upm.miw.iwvg_devops.code.Search;
 import org.junit.jupiter.api.Test;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class SearchTest {
@@ -18,6 +21,11 @@ class SearchTest {
     void testNotFindFirstProperFractionByUserId() {
         Fraction fraction = new Fraction(0, 1);
         assertFalse(new Search().findFirstProperFractionByUserId("3").isEquivalent(fraction));
+    }
+
+    @Test
+    void testFindUserIdByAllProperFraction() {
+        assertEquals(List.of(), new Search().findUserIdByAllProperFraction().collect(Collectors.toList()));
     }
 
 }
